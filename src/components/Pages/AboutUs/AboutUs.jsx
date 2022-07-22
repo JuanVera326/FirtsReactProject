0b543bfea0333f9e9252c3ccf092c15e5e749e31
JuanVera326 <jjvera86@misena.edu.co>
2022-07-22 17:51:36 -0500

    Ejercicio 2

--- src/components/Pages/AboutUs/AboutUs.jsx
+++ src/components/Pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from '../../UI/Input/Input'
 
 export const AboutUs = () => {
+
+  const [list, setList] = useState([]);
+    const name = (e) =>{
+      
+    }
+
   return (
     <div className='section2'>
         <h1>About Us</h1>
+
+        <div className='section4'>
+          <Input tp="text" textInput="Escriba aquí..." style="inputAPI" valid="true" cEvent={name}></Input>
+
+          <ul>
+
+          </ul>
+        </div>
     </div>
   )
 }
