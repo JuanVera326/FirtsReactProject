ae4ffe99500b5d221e03bdf5f8c62bcb6b7a304b
JuanVera326 <jjvera86@misena.edu.co>
2022-07-23 16:15:45 -0500

    Ejercicio

--- src/components/Pages/AboutUs/AboutUs.jsx
+++ src/components/Pages/AboutUs/AboutUs.jsx
@@ -3,21 +3,27 @@ import { Input } from '../../UI/Input/Input'
 
 export const AboutUs = () => {
 
-  const [list, setList] = useState([]);
-    const name = (e) =>{
-      
-    }
+  const [arr, setArr] = useState([]);
 
+  const names = (e) => {
+    if (e.key === 'Enter') {
+      let tempArr = arr;
+      tempArr.push(e.target.value);
+      setArr(tempArr);
+    }
+  }
+    
   return (
     <div className='section2'>
         <h1>About Us</h1>
 
         <div className='section4'>
-          <Input tp="text" textInput="Escriba aquí..." style="inputAPI" valid="true" cEvent={name}></Input>
-
-          <ul>
-
-          </ul>
+          <Input tp="text" textInput="Escriba aquí..." style="inputAPI" valid="true" event={names}></Input>
+          <>
+              {
+                arr.map((item) => (<li>{item}</li>))
+              }
+          </>
         </div>
     </div>
   )
